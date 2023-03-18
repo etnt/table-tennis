01bb321f909331211540a7bcf83b4ad691877ef4
Torbjörn Törnkvist <kruskakli@gmail.com>
2023-03-18 23:30:47 +0100

    Defining default and readonly values for Player form

--- lib/table_tennis/app/player.ex
+++ lib/table_tennis/app/player.ex
@@ -3,18 +3,20 @@ defmodule TableTennis.App.Player do
   import Ecto.Changeset
 
   schema "players" do
-    field :lost, :integer
+    field :lost, :integer, default: 0
     field :name, :string
-    field :rating, :integer
-    field :won, :integer
+    field :rating, :integer, default: 1000
+    field :won, :integer, default: 0
 
-    timestamps()
+    timestamps
   end
 
   @doc false
   def changeset(player, attrs) do
     player
-    |> cast(attrs, [:name, :won, :lost, :rating])
-    |> validate_required([:name, :won, :lost, :rating])
+    #|> cast(attrs, [:name, :won, :lost, :rating])
+    |> cast(attrs, [:name])
+    #|> validate_required([:name, :won, :lost, :rating])
+    |> validate_required([:name])
   end
 end
